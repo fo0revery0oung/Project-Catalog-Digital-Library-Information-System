c31674665a67a7e060885db1509765020a2e58ec
fo0revery0oung <leylaaars@student.ub.ac.id>
2025-12-13 13:44:21 +0700

    Delete model directory

--- model/Author.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Author {
-    private String name;
-    public Author(String name) { this.name = name; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    @Override public String toString() { return name; }
-}
